a21499cd5edb673d06e4926ae3587ff6c2e78f74
Franklin Hyriol <franklin.razafinandrasana@pulse.mg>
2025-09-19 13:16:17 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,21 @@ import { useEffect, useRef } from "react";
 import { useColumnsStore } from "./stores/ColumnStore";
 import { useGameStatsStore } from "./stores/GameStatsStore";
 import { GameSurface } from "./components/GameSurface/GameSurface";
-import NewGame from "./components/NewGame/NewGame";
-import Popup from "./components/Popup/Popup";
 import { usePopupStore } from "./stores/PopupStore";
-import PauseGame from "./components/PauseGame/PauseGame";
-import { Level } from "./types";
-
-import ConfirmAction from "./components/ConfirmAction/ConfirmAction";
-
-import GameWon from "./components/GameWon/GameWon";
-import HelpPopup from "./components/HelpPopup/HelpPopup";
-import GameOver from "./components/GameOver/GameOver";
+import PopupManager from "./components/PopupManager/PopupManager";
 import "./app.css";
 
 function App() {
-  const initGame = useColumnsStore((state) => state.initGame);
   const columns = useColumnsStore((state) => state.columns);
-  const restartGame = useColumnsStore((state) => state.restartGame);
   const isGameWon = useColumnsStore((state) => state.isGameWon);
   const isGameOver = useColumnsStore((state) => state.isGameOver);
 
   const initialColumnsLength = useRef(columns.length);
-  const startChrono = useGameStatsStore((state) => state.start);
   const pauseChrono = useGameStatsStore((state) => state.pause);
   const resumeChrono = useGameStatsStore((state) => state.resume);
-  const resetStats = useGameStatsStore((state) => state.reset);
 
   const popupType = usePopupStore((state) => state.type);
   const openPopup = usePopupStore((state) => state.open);
-  const closePopup = usePopupStore((state) => state.close);
 
   // Logique de démarrage de l'application
   useEffect(() => {
@@ -67,94 +53,14 @@ function App() {
     }
   }, [isGameOver, openPopup]);
 
-  const handlePlay = (level: Level) => {
-    resetStats();
-    initGame(level);
-    startChrono();
-    closePopup();
-  };
-
-  // Exécute le redémarrage
-  const executeRestart = () => {
-    resetStats();
-    restartGame();
-    startChrono();
-    closePopup(); // Ferme le popup de confirmation
-  };
-
   return (
     <>
-      <Header onRestart={() => openPopup("confirmRestart")} onHelp={() => openPopup("help")} />
+      <Header
+        onRestart={() => openPopup("confirmRestart")}
+        onHelp={() => openPopup("help")}
+      />
       <GameSurface />
-
-      {/* Popup pour une nouvelle partie */}
-      <Popup
-        open={popupType === "new"}
-        setOpen={closePopup}
-        closeOnOverlayClick={false} // On ne peut pas fermer en cliquant à côté
-      >
-        <NewGame onCancel={closePopup} onPlay={handlePlay} />
-      </Popup>
-
-      {/* Popup pour le jeu en pause */}
-      <Popup
-        open={popupType === "pause"}
-        setOpen={closePopup}
-        closeOnOverlayClick={true}
-      >
-        <PauseGame
-          onContinue={closePopup}
-          onRestart={() => openPopup("confirmRestart")}
-          onNewGame={() => openPopup("new")}
-        />
-      </Popup>
-
-      {/* Popup de confirmation pour recommencer */}
-      <Popup
-        open={popupType === "confirmRestart"}
-        setOpen={closePopup}
-        closeOnOverlayClick={true}
-      >
-        <ConfirmAction
-          title="Recommencer la partie ?"
-          message="Êtes-vous sûr ? Votre progression sur cette partie sera perdue."
-          onConfirm={executeRestart}
-          onCancel={closePopup}
-        />
-      </Popup>
-
-      {/* Popup de victoire */}
-      <Popup
-        open={popupType === "gameWon"}
-        setOpen={closePopup}
-        closeOnOverlayClick={false}
-      >
-        <GameWon
-          onNewGame={() => openPopup("new")}
-          onRestart={() => executeRestart()}
-        />
-      </Popup>
-
-      {/* Popup de Game Over */}
-      <Popup
-        open={popupType === "gameOver"}
-        setOpen={closePopup}
-        closeOnOverlayClick={false}
-      >
-        <GameOver
-          onNewGame={() => openPopup("new")}
-          onRestart={() => executeRestart()}
-        />
-      </Popup>
-
-      {/* Popup d'aide */}
-      <Popup
-        open={popupType === "help"}
-        setOpen={closePopup}
-        closeOnOverlayClick={true}
-      >
-        <HelpPopup onClose={closePopup} />
-      </Popup>
+      <PopupManager />
     </>
   );
 }
